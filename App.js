ca25f7f64d0ff410d00fb400d3f80b942384a081
haorup <haochemuh@gmail.com>
2024-09-10 14:48:18 -0700

    added an controlled InputText component.

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput,Text, View } from 'react-native';
+import  Header  from './Component/Header';
+import { useState } from 'react';
 
 export default function App() {
   const appName = 'Mobile Dev';
+  const [text, setText] = useState('')
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}</Text>
+
       <StatusBar style="auto" />
+      <Header name={appName} version='2' author='Hao'/>
+      <TextInput
+      style={{height:40}}
+      autocorrect={true}
+      placeholder='Type here'
+      value={text}
+      onChangeText={(newText)=>setText(newText)}>
+      </TextInput>
+      <Text>{text}</Text>
     </View>
   );
 }
