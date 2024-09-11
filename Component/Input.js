a9d653b5b53bef20de824e526af3b55ec39eb6af
haorup <haochemuh@gmail.com>
2024-09-10 22:25:33 -0700

    Used textCount as a state to track the text count, otherwise it won't rerender when the input field loses focus. Use useEffect to track the text and focus state and set the textCount when these two values change.

--- Component/Input.js
+++ Component/Input.js
@@ -1,37 +1,45 @@
 import { View, TextInput, Text } from 'react-native'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import React from 'react'
 
 
 const Input = (prop) => {
   const [text, setText] = useState('');
-  const ifTextInputFocus = useRef();
+  const [textCount, setTextCount] = useState('');
+  const [isFocused, setIsFocused] = useState(true);
 
-  const handleTextCount = () => {
-    if (!ifTextInputFocus.current || !(ifTextInputFocus.current.isFocused())) {
-      return '';
-    } else {
-      if (text) {
-        return text.length;
+  useEffect(() => {
+    const updateTextCount = () => {
+      if (!isFocused) { // If the input is not focused
+        if (!text) {
+          setTextCount('');
+        } else if (text.length >= 3) {
+          setTextCount('Thank you');
+        } else {
+          setTextCount('Please type more than 3 characters');
+        }
       } else {
-        return '';
-    }
-  }
-}
+        setTextCount(text ? text.length.toString() : ''); // If the input is focused
+      }
+    };
+    updateTextCount();
+  }, [text, isFocused]); // Only re-run the effect if the text or isFocused value changes
+
 
   return (
     <View>
       <TextInput
-        ref={ifTextInputFocus}
         style={{ borderBottomWidth: 1, borderBottomColor: 'black' }}
         autocorrect={true}
         placeholder='Type here'
         value={text}
         onChangeText={(newText) => setText(newText)}
-        autoFocus={prop.ifFocus}>
+        autoFocus={prop.ifFocus}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}>
       </TextInput>
       <Text>{text}</Text>
-      <Text>{handleTextCount()}</Text>
+      <Text>{textCount}</Text>
     </View>
   )
 }
