1f554d1372e40c26f451d2908a2ad2a8215748ab
haorup <haochemuh@gmail.com>
2024-09-10 21:53:15 -0700

    use isFocused method to access the focus prop of textinput, implemented a function to return the text count, and called in the text component.

--- Component/Input.js
+++ Component/Input.js
@@ -1,23 +1,37 @@
 import { View, TextInput, Text } from 'react-native'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import React from 'react'
 
 
 const Input = (prop) => {
-    const [text, setText] = useState('');
+  const [text, setText] = useState('');
+  const ifTextInputFocus = useRef();
+
+  const handleTextCount = () => {
+    if (!ifTextInputFocus.current || !(ifTextInputFocus.current.isFocused())) {
+      return '';
+    } else {
+      if (text) {
+        return text.length;
+      } else {
+        return '';
+    }
+  }
+}
 
   return (
     <View>
       <TextInput
-      style={{borderBottomWidth:1, borderBottomColor:'black'}}
-      autocorrect={true}
-      placeholder='Type here'
-      value={text}
-      onChangeText={(newText)=>setText(newText)}
-      autoFocus={prop.ifFocus}>
+        ref={ifTextInputFocus}
+        style={{ borderBottomWidth: 1, borderBottomColor: 'black' }}
+        autocorrect={true}
+        placeholder='Type here'
+        value={text}
+        onChangeText={(newText) => setText(newText)}
+        autoFocus={prop.ifFocus}>
       </TextInput>
       <Text>{text}</Text>
-      <Text>{text ? text.length : ''}</Text>
+      <Text>{handleTextCount()}</Text>
     </View>
   )
 }
