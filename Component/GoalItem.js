b2b1e979bb40ca1b83b6b9394e674deb0823871f
haorup <haochemuh@gmail.com>
2024-10-01 19:18:32 -0700

    Q2: passed navigation prop from Home.js to GoalItem.js, make use of the navigation in GoalItem.js

--- Component/GoalItem.js
+++ Component/GoalItem.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 export default function GoalItem({ goalObj,
-    goalDeleteHandler, goalPressHandler }) {
+    goalDeleteHandler, passedNavigation }) {
+
     function handleDelete() {
         goalDeleteHandler(goalObj.id);
     }
 
-    function handlePress() {
-        goalPressHandler(goalObj);
+    // make use of the passed navigation from Home.js
+    function handleNavigation() {
+        passedNavigation.navigate('GoalDetails',
+            { goalData: goalObj });
     }
     return (<View
         style={styles.textBackgroundStyle}>
         <Text style={styles.text}>{goalObj.text}</Text>
         <Button title='X' color='red' onPress={() => handleDelete()} />
-        <Button title='I' color='green' onPress={() => handlePress()} />
+        <Button title='I' color='green' onPress={() => handleNavigation()} />
     </View>)
 }
 
