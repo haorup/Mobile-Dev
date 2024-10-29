b0f36c0a3e176995369a3d6b8e71090e81e7a0b4
peihao <pei.h@northeastern.edu>
2024-10-29 14:16:34 -0700

    implement the handle signup function in signup screen

--- Component/Login.js
+++ Component/Login.js
@@ -1,13 +1,24 @@
 import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
+import PressButton from './PressButton'
 
-export default function Login() {
+export default function Login({navigation}) {
+    function handleNavigation() {
+        navigation.replace('Signup')
+    }
   return (
     <View>
       <Text>Email address</Text>
       <TextInput placeholder='Email' />
       <Text>Password</Text>
       <TextInput placeholder='Password' />
+      <PressButton>
+        <Text>Login</Text>
+      </PressButton>
+      <PressButton
+      passedOnPress={handleNavigation}>
+        <Text>New user? Create an account</Text>
+      </PressButton>
     </View>
   )
 }
--- Component/Signup.js
+++ Component/Signup.js
@@ -1,31 +1,53 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import PressButton from './PressButton'
 import { useNavigation } from '@react-navigation/native'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../Firebase/firebaseSetup'
 
 export default function Signup() {
     const navigation = useNavigation()
+    const [valueVar, setValueVar] = useState('')
+    const [passwordVar, setPasswordVar] = useState('')
 
     function handleNavigation() {
         navigation.replace('Login')
     }
-  return (
-    <View style={styles.container}>
-      <Text>Email address</Text>
-      <TextInput placeholder='Email' />
-      <Text>Password</Text>
-      <TextInput placeholder='Password' />
-      <Text>Confirm Password</Text>
-      <TextInput placeholder='Confirm Password' />
-      <PressButton>
-        <Text>Register</Text>
-      </PressButton>
-      <PressButton
-      passedOnPress={handleNavigation}>
-        <Text>Already registered? Login</Text>
-      </PressButton>
-    </View>
-  )
+
+    // check if
+    const handleSignUp = async () => {
+        try {
+        if (valueVar.trim() !== '' &&
+            passwordVar.trim() !== '')
+            createUserWithEmailAndPassword(auth, valueVar, passwordVar);
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Email address</Text>
+            <TextInput placeholder='Email'
+                value={valueVar}
+                onChangeText={setValueVar} />
+            <Text>Password</Text>
+            <TextInput placeholder='Password'
+                value={passwordVar}
+                onChangeText={setPasswordVar} />
+            <Text>Confirm Password</Text>
+            <TextInput placeholder='Confirm Password' />
+
+            <PressButton passedOnPress={handleSignUp}>
+                <Text>Register</Text>
+            </PressButton>
+
+            <PressButton
+                passedOnPress={handleNavigation}>
+                <Text>Already registered? Login</Text>
+            </PressButton>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
